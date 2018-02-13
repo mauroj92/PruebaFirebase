46addc36bd3192b8221f789489c94cd7cb8558d4
MauroJ <mauroojuarez@gmail.com>
2018-02-13 12:57:43 -0300

    Guardar y recuperar imagen del Storage

--- app/src/main/java/com/example/maurojuarez/firebase/MainActivity.java
+++ app/src/main/java/com/example/maurojuarez/firebase/MainActivity.java
@@ -1,23 +1,36 @@
 package com.example.maurojuarez.firebase;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     EditText edtCantidad;
     TextView txvCantidad;
     Button btnCantidad;
+    ImageView imvFoto;
 
     DatabaseReference referencia;
 
@@ -26,11 +39,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imvFoto = (ImageView) findViewById(R.id.imvFoto);
         edtCantidad = (EditText) findViewById(R.id.edtCantidad);
         txvCantidad = (TextView) findViewById(R.id.txvCantidad);
         btnCantidad = (Button) findViewById(R.id.btnCantidad);
         btnCantidad.setOnClickListener(this);
 
+        //Prueba de DATABASE
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         referencia = database.getReference("persona");
 
@@ -47,6 +62,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             }
         });
+
+        //Prueba de STORAGE
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReferenceFromUrl("gs://probando-firebase-50610.appspot.com").child("ANDROID.png");
+        try {
+            final File localFile = File.createTempFile("images", "jpg");
+            storageRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                    imvFoto.setImageBitmap(bitmap);
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                }
+            });
+        } catch (IOException e ) {}
     }
 
     @Override
